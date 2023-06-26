303647e7df2ec8413cabefb5e2a22d4079d53d88
kiyosaki-cyber <134641062+kiyosaki-cyber@users.noreply.github.com>
2023-06-26 05:15:06 +0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import Navbar from "./components/nav"
+import Form from "./components/form"
+import './App.css'
+
+function App() {
+
+  return (
+    <div>
+     <Navbar/> 
+    
+    <Form />
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/nav.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+export default function nav(){
+    return(
+        <div className="header">
+            <img src="../src/img/memes-faces-11550716295igwesduevi.png" alt="header img" className="header--image" />
+            <h2>Meme Generator</h2>
+     
+        </div>
+        
+      
+    )
+}
